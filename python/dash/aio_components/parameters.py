0a7291bd49f713d03687c5975f109590f6005baf
Joshua Santana Colon <joshua.santana-colon@capgemini.com>
2022-09-08 17:37:22 +0200

    Fixed updates occuring before predict button is triggered

--- python/dash/aio_components/parameters.py
+++ python/dash/aio_components/parameters.py
@@ -15,28 +15,28 @@ from dash import (
 
 class ParameterStoreAIO(html.Div):
     class ids:
-        store = lambda parameter:{
-            "component":"ParameterStoreAIO",
-            "subcomponent":"store",
-            "aio_id":parameter
+        store = lambda parameter: {
+            "component": "ParameterStoreAIO",
+            "subcomponent": "store",
+            "aio_id": parameter,
         }
-        
-        input = lambda parameter:{
-            "component":"ParameterStoreAIO",
-            "subcomponent":"input",
-            "aio_id":parameter
+
+        input = lambda parameter: {
+            "component": "ParameterStoreAIO",
+            "subcomponent": "input",
+            "aio_id": parameter,
         }
-        
-        submit_button = lambda parameter:{
-            "component":"ParameterStoreAIO",
-            "subcomponent":"submit_button",
-            "aio_id":parameter
+
+        submit_button = lambda parameter: {
+            "component": "ParameterStoreAIO",
+            "subcomponent": "submit_button",
+            "aio_id": parameter,
         }
-        
-        container = lambda parameter:{
-            "component":"ParameterStoreAIO",
-            "subcomponent":"container",
-            "aio_id":parameter
+
+        container = lambda parameter: {
+            "component": "ParameterStoreAIO",
+            "subcomponent": "container",
+            "aio_id": parameter,
         }
 
     ids = ids
@@ -86,26 +86,26 @@ class ParameterStoreAIO(html.Div):
                 "margin-top": "10px",
             },
         )
+
     @callback(
         Output(ids.store(MATCH), "data"),
         Input(ids.input(MATCH), "value"),
         Input(ids.submit_button(MATCH), "n_clicks"),
     )
-    def update_parameter_store(parameter_input:str, n_clicks: int):
-        
-        if parameter_input:
-        
-            changed_item = [p["prop_id"] for p in callback_context.triggered][0]
-            
+    def update_parameter_store(parameter_input: str, n_clicks: int):
+
+        changed_item = [p["prop_id"] for p in callback_context.triggered][0]
+
+        if parameter_input and "submit_button" in changed_item:
             if "scenario" in changed_item:
                 scenario = parameter_input.replace(" ", "").split(",")
-                
+
                 scenario = [int(x) for x in scenario]
-                
+
                 return scenario
-            
+
             else:
                 return parameter_input
-            
+
         else:
             raise exceptions.PreventUpdate
