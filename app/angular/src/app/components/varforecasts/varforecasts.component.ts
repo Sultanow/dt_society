938a5e2ed63fb70e4662eba994da4819c7e3c8ac
Simon <simon.brand99@gmail.com>
2023-01-10 13:47:43 +0100

    fixed uploaddialog for var forecast

--- app/angular/src/app/components/varforecasts/varforecasts.component.ts
+++ app/angular/src/app/components/varforecasts/varforecasts.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Selections } from 'src/app/types/Datasets';
 
 @Component({
   selector: 'app-varforecasts',
   templateUrl: './varforecasts.component.html',
-  styleUrls: ['./varforecasts.component.css']
+  styleUrls: ['./varforecasts.component.css'],
 })
 export class VarforecastsComponent implements OnInit {
+  constructor(private dataService: DataService) {}
 
-  constructor() { }
+  selections: Selections = {
+    datasets: [],
+    selectedDataset: undefined,
+  };
 
   ngOnInit(): void {
+    this.dataService.currentSelections.subscribe((data) => {
+      this.selections = data;
+    });
   }
-
 }
