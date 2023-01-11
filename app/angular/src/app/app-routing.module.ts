ca4ea6de7575ce46fc0a808a1b0342ad47f48231
Simon <simon.brand99@gmail.com>
2023-01-11 12:35:23 +0100

    refactored routes and added routes for var forecasts for caching

--- app/angular/src/app/app-routing.module.ts
+++ app/angular/src/app/app-routing.module.ts
@@ -1,10 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CorrelationsComponent } from './components/correlations/correlations.component';
+import { ProphetscenariosComponent } from './components/forecast/prophetscenarios/prophetscenarios.component';
+import { VarMapComponent } from './components/forecast/var-map/var-map.component';
+import { VectorautoregressionComponent } from './components/forecast/vectorautoregression/vectorautoregression.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { VarforecastsComponent } from './components/varforecasts/varforecasts.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/overview',
+    pathMatch: 'full',
+    data: { reuse: true },
+  },
+  {
+    path: 'overview',
+    component: OverviewComponent,
+    pathMatch: 'full',
+    data: { reuse: true },
+  },
+  {
+    path: 'correlations',
+    component: CorrelationsComponent,
+    pathMatch: 'full',
+    data: { reuse: true },
+  },
+  {
+    path: 'forecasts/var',
+    component: VarforecastsComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'graph',
+      },
+      {
+        path: 'graph',
+        component: VectorautoregressionComponent,
+        data: { reuse: true },
+      },
+      {
+        path: 'map',
+        component: VarMapComponent,
+        data: { reuse: true },
+      },
+    ],
+  },
+  {
+    path: 'forecasts/prophet',
+    component: ProphetscenariosComponent,
+    pathMatch: 'full',
+    data: { reuse: true },
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
